13de8c8100813653e599e595b712e287d03c843c
duyvc <vucongduy192@gmail.com>
2020-01-15 00:55:27 +0700

    add save image output

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+### Overview
+The programs in this repository train and use a fully convolutional neural network to take an image and classify 
+its pixels. The network is transfer-trained basing on the VGG-16 model using the approach described in this paper 
+by Jonathan Long et al. 
+
 ### Source kitti dataset + VGG base
 Dataset + VGG url: https://drive.google.com/file/d/1je7YaKnb3dhEQ33tvCeOeqt6vy2M_Ubv/view?usp=sharing
 
@@ -9,7 +14,8 @@ Download and extract to root directory
 * CuDNN 7.4.1
 
 ### Installation guide
-https://gist.github.com/bogdan-kulynych/f64eb148eeef9696c70d485a76e42c3a
+Installation follow this guide https://gist.github.com/bogdan-kulynych/f64eb148eeef9696c70d485a76e42c3a
+with some points of attention:
 * Install specific cuda version
 ```bash
 sudo apt install -y cuda-10-0    
@@ -18,5 +24,12 @@ sudo apt install -y cuda-10-0
 ```bash 
 sudo apt-cache policy libcudnn7
 ```
+* Remember to install same specific version for libcudnn7 and libcudnn7-dev
+
 * Check compatible version tensorflow-gpu
+
 ![GPU setup environment](./assets/GPUsetup.png)
+
+### Reference: 
+* https://github.com/ljanyst/image-segmentation-fcn
+* https://nanonets.com/blog/how-to-do-semantic-segmentation-using-deep-learning/
--- test.py
+++ test.py
@@ -26,7 +26,7 @@ def sample_generator(samples, img_size, batch):
             image = cv2.resize(cv2.imread(image_file), img_size)
             images.append(image.astype(np.float32))
             names.append(os.path.basename(image_file))
-        yield np.array(images)
+        yield np.array(images), names
 
 
 # Specify all directory paths
@@ -39,6 +39,7 @@ sample_paths = glob(samples_dir)
 
 batch_size = 4
 image_size = (576, 160)
+label_colors = {0: np.array([0, 0, 0]), 1: np.array([255, 0, 255])}
 
 
 def main():
@@ -66,13 +67,15 @@ def main():
             image_softmax = tf.nn.softmax(image)
             session.run(image_softmax)
             predict = image_softmax.eval()
-            print(image_softmax.eval())
 
-            image_reshaped = image_softmax.eval().reshape(batch_size, 160, 576, 2)
-            img = np.argmax(image_reshaped[2], axis=2)
-
-            plt.imshow(img, cmap='gray')
-            plt.show()
+            image_reshaped = image_softmax.eval().reshape(batch_size, image_size[1], image_size[0], 2)
+            for idx in range(image_reshaped.shape[0]):
+                image_labelled = np.argmax(image_reshaped[idx], axis=2)
+                image_save = np.zeros((image_size[1], image_size[0], 3))
+                for val, color in label_colors.items():
+                    label_mask = image_labelled == val
+                    image_save[label_mask] = color
+                cv2.imwrite(output_dir + '/' + names[idx], image_save)
             break
 
 
