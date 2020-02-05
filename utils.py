1742432b2531e3ba0a2dc6f965042ebe0d1d2976
duyvc <vucongduy192@gmail.com>
2020-02-05 07:42:36 +0700

    apply to voc2012

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 from source_kitti import KittiSource
+from source_voc import VOCSource
 from fcnvgg import FCNVGG
 
 
-def load_data_source():
+def load_kitti_source():
     return KittiSource()
+
+
+def load_voc_source():
+    return VOCSource()
 
 
 def load_fcnvgg(session, num_classes):
